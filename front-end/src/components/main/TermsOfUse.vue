b055d93bab5e5036da0ea24017bf38047febcfa1
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-10-05 19:06:17 -0700

    added more terms of use

--- front-end/src/components/main/TermsOfUse.vue
+++ front-end/src/components/main/TermsOfUse.vue
@@ -2,16 +2,28 @@
     <!-- TERMS OF USE COMPONENT -->
 
     <div class="container text-center p-5">
-        <h2>Terms of Use</h2>
+        <h2 id="top">Terms of Use</h2>
 
         <p class="mb-4">Last revised on October 03, 2022</p>
 
         <p>Welcome to Eat, Drink, What! ("us", "we", the "Company", the "Service", or "Eat, Drink, What!"). Together, you and Eat, Drink, What! may be referred to as the "Parties" or separately as "Party."</p>
 
+        <ul>
+            <li><a href="#one">Acceptance of the Terms of Use Agreement</a></li>
+            <li><a href="#two">Eligibility</a></li>
+            <li><a href="#three">Your Account</a></li>
+            <li><a href="#four">Modifying the Service and Termination</a></li>
+            <li><a href="#five">Safety; Your Interactions With Other Members</a></li>
+            <li><a href="#six">Rights Eat, Drink, What! Grants You</a></li>
+            <li><a href="#seven">Rights You Grant Eat, Drink, What!</a></li>
+        </ul>
+
+
+
         <hr>
 
         <!-- FIRST SECTION -->
-        <h3>1) Acceptance of the Terms of Use Agreement</h3>
+        <h3 id="one">1) Acceptance of the Terms of Use Agreement</h3>
 
         <p>By creating an account with Eat, Drink, What!, whether through a mobile device, mobile application or computer, you agree to be bound by:</p>
 
@@ -31,18 +43,104 @@
 
         <p>If you continue to use the Service after the changes become effective, then you agree to the revised Agreement. You agree that this revised Agreement shall supersede any prior agreements (except as specifically stated herein), and shall govern your entire relationship with Eat, Drink, What!, including but not limited to events, agreements, and conduct preceding your acceptance of this Agreement.</p>
 
+        <a href="#top">Back to Top</a>
         <hr>
 
         <!-- SECOND SECTION -->
-        <h3>2) Eligibility</h3>
+        <h3 id="two">2) Eligibility</h3>
 
         <p>You must be at least 18 years of age to create an account on Eat, Drink, What! and use the Service. By creating an account and using the Service, you represent and warrant that:</p>
 
         <ul>
             <li>- you can form a binding contract with Eat, Drink, What!</li>
-            <li> - you are not a person who is barred from using the Service under the laws of the United States of America or any other applicable jurisdiction (for example, you do not appear on the U.S. Treasury Department's list of Specially Designated Nationals or face any other similar prohibition)</li>
-            <li> - you will comply with this Agreement and all applicable local, state, national, and international laws, rules, and regulations. Including without limitation, privacy laws, intellectual property laws, anti-spam laws, and regulatory requirements</li>
+            <li>- you are not a person who is barred from using the Service under the laws of the United States of America or any other applicable jurisdiction (for example, you do not appear on the U.S. Treasury Department's list of Specially Designated Nationals or face any other similar prohibition)</li>
+            <li>- you will comply with this Agreement and all applicable local, state, national, and international laws, rules, and regulations. Including without limitation, privacy laws, intellectual property laws, anti-spam laws, and regulatory requirements</li>
+            <li>- you have not committed, been convicted of or pled no contest to a felony, a sex crime, or any crime involving violence or a threat of violence; unless you have received clemency for a non-violent crime and we have determined that you are not likely to pose a threat to other users of our Service, and that you are not required to register as a sex offender with any state, federal, or local sex offender registry</li>
+            <li>- you will use the latest version of the Service on all platforms</li>
+            <li>- you do not have more than one account on the Service</li>
+            <li>- you have not previously been removed from the Service by us; unless you have our express written permission to create a new account</li>
         </ul>
+
+        <p>If at any time you cease to meet these requirements, all authorizations to access our Service or systems is automatically revoked and your account will be immediately deleted.</p>
+
+
+        <a href="#top">Back to Top</a>
+        <hr>
+
+        <!-- THIRD SECTION -->
+        <h3 id="three">3) Your Account</h3>
+
+        <p>In order to use Eat, Drink, What!, you may sign in with email and password. For more information regarding the information we collect from you and how we use it, please consult our <a href="#privacy">Privacy Policy</a>.</p>
+
+        <p>You are responsible for maintaining the confidentiality of your login credentials you use to sign up for Eat, Drink, What!, and you are solely responsible for all activities that occur under those credentials. If you think someone has gained access to your account, please immediately <a href="contact.html">contact us</a>.</p>
+
+
+
+        <a href="#top">Back to Top</a>
+        <hr>
+
+        <!-- FOURTH SECTION -->
+        <h3 id="four">4) Modifying the Service and Termination</h3>
+
+        <p>Eat, Drink, What! is always striving to improve the Service and bring you additional functionality that you will find engaging and useful. This means we may add new Service features or enhancements from time to time as well as remove some features. If these actions do not materially affect your rights or obligations, we may not provide you with notice before taking them. We may even suspend the Service entirely, in which event we will notify you in advance unless extenuating circumstances, such as safety or security concerns, prevent us from doing so.</p>
+
+        <p>You may terminate your account at any time, for any reason, by following the instructions in "Questions" in the Service. Eat, Drink, What! may terminate your account at any time without notice if it believes that you have violated this Agreement. After your account is terminated, this Agreement will terminate, except that the following provisions will still apply to you and Eat, Drink, What! (the "Parties"): <a href="#four">Section 4</a>, <a href="#five">Section 5</a>, <a href="#six">Section 6</a></p>
+        <!-- FINISH LISTING THE SECTIONS -->
+
+
+
+        <a href="#top">Back to Top</a>
+        <hr>
+
+        <!-- FIFTH SECTION -->
+        <h3 id="five">5) Safety; Your Interactions With Other Members</h3>
+
+        <p>You agree to treat other users in a courteous and respectful manner, both on and off our Service, and to be respectful when communicating with any of our customer care representatives or other employees.</p>
+
+        <p>Though Eat, Drink, What! strives to encourage a respectful member experience <strong>Eat, Drink, What! is not responsible for the conduct of any member on or off the Service.</strong></p>
+
+        <p><strong>You agree to use caution in all interactions with other members. In addition, you agree to review and follow Eat, Drink, What!'s <a href="#safety">Safety Tips</a> prior to using the Service.</strong></p>
+
+        <p><strong>You are solely responsible for your interactions with other members. You understand that Eat, Drink, What! does not conduct criminal background checks on its members or otherwise inquires into the background of its members. Eat, Drink, What! makes no representations or warranties as to the conduct or compatibility of members.</strong></p>
+
+        <p>Eat, Drink, What! reserves the right to conduct - and you authorize Eat, Drink, What! to conduct - any criminal background check or other screenings (such as sex offender register searches) at any time using available public records obtained by it or with the assistance of a consumer reporting agency. And you agree that any information you provide may be used for that purpose.</p>
+
+
+
+        <a href="#top">Back to Top</a>
+        <hr>
+
+        <!-- SIXTH SECTION -->
+        <h3 id="six">6) Rights Eat, Drink, What! Grants You</h3>
+
+        <p>Eat, Drink, What! grants you a personal, worldwide, royalty-free, non-assignable, non-exclusive, revocable, and non-sublicensable license to access and use the Service. This license is for the sole purpose of letting you use and enjoy the Service's benefits as intended by Eat, Drink, What! and permitted by this Agreement. This license and any authorization to access the Service are automatically revoked in the event that you do any of the following:</p>
+
+        <ul>
+            <li>- use the Service or any content contained in the Service for any commercial purposes without our written consent</li>
+            <li>- copy, modify, transmit, create any derivative works from, make use of, or reproduce in any way any copyrighted material, images, trademarks, trade names, service marks, or other intellectual property, content or proprietary information accessible through the Service without Eat, Drink, What!'s prior written consent</li>
+            <li>- express or imply that any statements you make are endorsed by Eat, Drink, What!</li>
+            <li>- use any robot, bot, spider, crawler, scraper, site search/retrieval application, proxy or other manual or automatic device, method or process to access, retrieve, index, "data mine", or in any way reproduce or circumvent the navigational structure or presentation of the Service or its contents</li>
+            <li>- use the Service in any way that could interfere with, disrupt or negatively affect the Service or the servers or networks connected to the Service</li>
+            <li>- upload viruses or other malicious code or otherwise compromise the security of the Service</li>
+            <li>- forge headers or otherwise manipulate identifiers in order to disguise the origin of any information transmitted to or through the Servce</li>
+            <li>- "frame" or "mirror" any part of the Service without Eat, Drink, What!'s prior written authorization</li>
+            <li>- use meta tags or code or other devices containing any reference to Eat, Drink, What! or the Service (or any trademark, trade name, service mark, logo or slogan of Eat, Drink, What!) to direct any person to any other website for any purpose</li>
+            <li>- modify, adapt, sublicense, translate, sell, reverse engineer, decipher, decompile, or otherwise disassemble any portion of the Service, or cause others to do so</li>
+            <li>- use or develop any third-party applications that interact with the Service or other members' content or information without our written consent</li>
+            <li>- use, access, or publish the Eat, Drink, What! application programming interface without our written consent</li>
+            <li>- probe, scan or test the vulnerability of our Service or any system or network</li>
+            <li>- encourage, promote, or agree to engage in any activity that violates this Agreement</li>
+        </ul>
+
+        <p>Eat, Drink, What! may investigate and take any available legal action in response to illegal and/or unauthorized uses of the Service, including termination of your account. Any software that we provide you may automatically download and install upgrades, updates, or other new features. You may be able to adjust these automatic downloads through your device's settings.</p>
+
+
+
+        <a href="#top">Back to Top</a>
+        <hr>
+
+        <!-- SEVENTH SECTION -->
+        <h3 id="seven">7) Rights You Grant Eat, Drink, What!</h3>
 
     </div>
 
