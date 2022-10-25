a093679248cb06fd766937d9b6cccb3631867a44
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-10-24 17:10:32 -0700

    added dummy event data

--- front-end/src/components/main/UserDashboard.vue
+++ front-end/src/components/main/UserDashboard.vue
@@ -1,3 +1,36 @@
 <template>
-    <h1>Dashboard</h1>
+    <div class="container text-center">
+
+        <h2 class="mt-5 text-center">Dashboard</h2>
+
+        <button class="btn btn-dark mt-3 mb-5">Add Event</button>
+
+        <section class="row">
+            <event-block :eventData="events"></event-block>
+        </section>
+    </div>
 </template>
+
+
+
+<script>
+    import EventBlock from '../utils/EventCard.vue';
+
+    export default {
+        components: {
+            EventBlock
+        },
+        data() {
+            return {
+                events: [
+                    {title: 'Event One', address: '207 Maple St'},
+                    {title: 'Event Two', address: '207 Maple St'},
+                    {title: 'Event Three', address: '207 Maple St'},
+                    {title: 'Event Four', address: '207 Maple St'},
+                    {title: 'Event Five', address: '207 Maple St'},
+                ]
+            }
+        }
+    }
+
+</script>
--- front-end/src/components/utils/EventCard.vue
+++ front-end/src/components/utils/EventCard.vue
@@ -1,5 +1,40 @@
 <template>
 
-    <!-- BOOTSTRAP CARDS AND VUE JS COURSE GAMESPOT HOME CARDS -->
+    <article class="col-lg-3 col-sm-12" v-for="(event, index) in eventData" :key="index">
+        <h4>{{event.title}}</h4>
+        <p>Date and time</p>
+        <p>{{event.address}}</p>
+        <p>Description: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente aliquam dolorem et quia facere dicta? Fuga nisi repellendus deserunt ipsa, unde cupiditate non culpa, repellat nihil harum soluta, cumque consectetur!</p>
+    </article>
+
+    <!-- If the event is today then reverse colors: black background and white text. -->
 
 </template>
+
+
+
+
+<script>
+
+    export default {
+        props: ['eventData']
+    }
+
+</script>
+
+
+
+
+<style scoped>
+
+    article {
+        border: 2px solid black;
+        padding: 10px;
+        text-align: center;
+    }
+
+    h4 {
+        text-decoration: underline;
+    }
+
+</style>
